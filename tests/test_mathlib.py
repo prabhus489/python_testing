be48963add25e09ebcc775e18b0c9d12617f459b
PRABHU SUNDARRAJ <43057160+prabhus489@users.noreply.github.com>
2019-05-12 14:51:22 +0530

    Update test_mathlib.py

--- tests/test_mathlib.py
+++ tests/test_mathlib.py
@@ -2,10 +2,10 @@ import pytest
 from python_codes.mathlib import calc_total, calc_multiply
 
 def test_calc_total():
-    total=mathlib.calc_total(4,5)
+    total=calc_total(4,5)
     assert total == 9
 
 def test_calc_multiply():
-    result=mathlib.calc_multiply(10,3)
+    result=calc_multiply(10,3)
     assert result == 30
 
